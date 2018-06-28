145c194471022eda9cb942f5ad57847414b7d84a
yulikexuan <yulikexuan@gmail.com>
2018-06-28 09:05:28 -0400

    Initial commit

---
+++ src/main/java/com/yulikexuan/daggerlab/DaggerlabApplication.java
@@ -0,0 +1,12 @@
+package com.yulikexuan.daggerlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaggerlabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DaggerlabApplication.class, args);
+    }
+}
